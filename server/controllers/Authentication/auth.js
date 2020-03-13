c3d4b6c69331ba403102b168b25deaf2d31aac68
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-03-13 13:18:06 +0200

    authentication code #10
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- server/controllers/Authentication/auth.js
+++ server/controllers/Authentication/auth.js
@@ -1,5 +1,45 @@
-const login = (req, res) => {
-  res.json({ login: 'logiiin' });
+const dbUser = require('./../../Models/Class/db_Users');
+
+const responseMessages = require('./../../Helpers/responseMessage');
+
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    dbUser
+      .getUserByEmail(email)
+      .then((result) => {
+        console.log(result);
+        if (result.length == 0) {
+          res.json(
+            responseMessages.FaildLoginMessage(null, ' Email or Pasword worng'),
+          );
+        }
+        const user = result[0];
+        const accessToken = jwt.sign(
+          { Name: user.name, role: user.role },
+          process.env.ACCESS_TOKEN_SECRET,
+        );
+
+        console.log(accessToken);
+        res.setHeader('Authentication', `Bearer ${accessToken}`);
+        res.json(
+          responseMessages.successMessage(
+            { token: accessToken },
+            'login successfully',
+          ),
+        );
+      })
+      .catch((err) => {
+        res.json({ status: 500, message: err });
+        return next(new Error(err));
+      });
+  } catch {
+    console.log('inside catch');
+  }
 };
 
 const logout = (req, res) => {
